a19fcf051ab005cbd853e49d6802e174c44d7e16
jacobecode <kuba.g.code.4@gmail.com>
2026-09-14 17:38:47 +0200

    Change package managing from py script to CI

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,12 +2,6 @@
 Post-gen logic for cookiecutter.
 """
 
-import os
-import sys
-
-# Add src to path so template_hooks can be imported
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
-
 from template_hooks.cleanup import run
 
 run("{{ cookiecutter.local_model }}")
