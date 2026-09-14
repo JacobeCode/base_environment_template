c1072355105a5e5b9dc032285f97600397608a17
jacobecode <kuba.g.code.4@gmail.com>
2026-09-14 17:08:24 +0200

    Adding env variable manipulation for checks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,11 @@
 Post-gen logic for cookiecutter.
 """
 
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "src"))
+
 from template_hooks.cleanup import run
 
 run("{{ cookiecutter.local_model }}")
